fab07867fa64ad3fbcb61ab5d66fe02511063865
Fabiano Moraes <32004353+masternetfmoraes@users.noreply.github.com>
2022-05-05 22:59:54 +0000

    commit 3

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 // import components
-import TopNavbar from './components/TopNavbar'
+import TopNavbar from './components/TopNavbar';
 import Carrossel from './components/Carrossel'
 import About from './components/About'
 import Team from './components/Team'
