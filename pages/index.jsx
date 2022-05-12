1ba53e1e17737d5f5d0e0bd6446bb5a9cefc72a6
Fabiano Moraes <32004353+masternetfmoraes@users.noreply.github.com>
2022-05-12 00:11:43 +0000

    Inserido Head/ Next. Cabeçalhos para indexar nos mecanimos de busca

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,6 +13,21 @@ import Footer from './components/Footer'
 export default function Home() {
   return (
     <>
+      <Head>
+        <title> Fabiano Moraes FullStack Developer , Father and Gammer</title>
+        <
+        meta name = "description"
+        content = "LawFirm This site was made using Nextjs" / >
+        <meta name="ABSTRACT" content="LawFirm website,Template Made using Replit and NextJS " />
+        <meta name="KEYWORDS" content="LawFirm, Lawyers, Law" />
+        <meta name="ROBOT" content="All" />
+        <meta name="robots" content="index, follow" />
+        <meta name="distribution" CONTENT="global" />
+        <meta name="copyright" content="LawFirm" />
+        <link rel="canonical" href="https://www.falwfirm.com" />
+        <link rel = "icon"
+        href = "/favicon.ico" />
+        </Head>
       <TopNavbar />
       <Carrossel />
       <About />
