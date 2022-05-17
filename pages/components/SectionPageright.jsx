1aa516d49ff273c326bb2f385da6cf52a4f0adb7
Fabiano Moraes <32004353+masternetfmoraes@users.noreply.github.com>
2022-05-17 20:43:32 +0000

    Commit section

--- pages/components/SectionPageright.jsx
+++ pages/components/SectionPageright.jsx
@@ -1,4 +1,5 @@
 import { Button, Container,Row,Col } from 'react-bootstrap';
+import styles from '../../styles/Home.module.css'
 export default function SectionPageright({title,description,image,bgcolor,textcolor,section }){
   return(
     <>
@@ -6,14 +7,14 @@ export default function SectionPageright({title,description,image,bgcolor,textco
       <Container fluid className={`${bgcolor} ${textcolor} my-5`}>
         <Row> 
           <Col sm={6} className="px-5">
-            <p className="h5 text-secondary mb-0">Subtitle here</p>
+            <p className="h5 text-secondary mb-0 mt-5">Subtitle here</p>
             <p className="display-4 text-dark mb-3 border-bottom border-secondary">{title}</p>
             
-           <p className="text-start">{description} </p>
+           <p className={`${styles.bgpoligons}`}>{description} </p>
             <Button className="my-3">See More </Button>
         </Col>
-          <Col sm={6} className="px-5">
-          <img src={image} alt="imagem" className="d-block w-100 rounded" />
+          <Col sm={6} className={`${styles.bgpoligon} px-5 d-sm-none d-md-block d-none d-sm-block `}>
+          <img src={image} alt="imagem" className="d-block w-100" />
         </Col>
         </Row>
       </Container>
