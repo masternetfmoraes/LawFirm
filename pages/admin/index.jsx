018d9400ce1968129e9aadb416ff243f4d6319a6
Fabiano Moraes <32004353+masternetfmoraes@users.noreply.github.com>
2022-06-20 03:36:31 +0000

    Acertando o menu lateral

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,22 +1,30 @@
 import { Container,Row,Col,Button } from 'react-bootstrap';
+import styles from '../../styles/Home.module.css'
+
 
 export default function index(){
   const divStyle = {
   color: 'white',
-  fontSize: "45px",
-  backgroundColor: 'black',
+  fontSize: "18.5px",
+  listStyle: "none"
 };
+  const li={
+    color:"white",
+    fontWeight:"300",
+    textTransform:"uppercase",
+    fontSize:"14px",
+    padding:"7px"
+  };
   return(<>
     <Container fluid>
       <Row>
         <Col sm={2} className="bg-dark text-white">
           <p className="h1 mt-5">Dashboard</p>
-          <ul className="list-group list-group-flush mb-5">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
+          <ul style={divStyle}>
+            <li style={li}><a href="">Home</a></li>
+            <li style={li}><a href="">User Profile</a></li>
+            <li style={li}><a href="">Tipography</a></li>
+            <li style={li}><a href="">Icons</a></li>
           </ul>
 
           
